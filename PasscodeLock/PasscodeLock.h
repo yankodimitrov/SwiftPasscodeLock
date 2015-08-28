80b6a79d2281f0cc2bb6f77753f59dd11db77e9d
Yanko Dimitrov <yanko@yankodimitrov.com>
2015-08-28 18:10:52 +0300

    Created new project

---
+++ PasscodeLock/PasscodeLock.h
@@ -0,0 +1,19 @@
+//
+//  PasscodeLock.h
+//  PasscodeLock
+//
+//  Created by Yanko Dimitrov on 8/28/15.
+//  Copyright © 2015 Yanko Dimitrov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PasscodeLock.
+FOUNDATION_EXPORT double PasscodeLockVersionNumber;
+
+//! Project version string for PasscodeLock.
+FOUNDATION_EXPORT const unsigned char PasscodeLockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PasscodeLock/PublicHeader.h>
+
+
