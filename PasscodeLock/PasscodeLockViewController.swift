7b838659aa4453e6026c7b9ea3b1c958b3430374
Chris Ziogas <ziogaschr@gmail.com>
2015-10-22 15:13:54 +0300

    Small fix for using self in closure

--- PasscodeLock/PasscodeLockViewController.swift
+++ PasscodeLock/PasscodeLockViewController.swift
@@ -238,8 +238,8 @@ public class PasscodeLockViewController: UIViewController, PasscodeLockTypeDeleg
         
         deleteSignButton?.enabled = true
         animatePlaceholders(placeholders, toState: .Inactive)
-        dismissPasscodeLock(lock, completionHandler: { _ in
-            successCallback?(lock: lock)
+        dismissPasscodeLock(lock, completionHandler: { [weak self] _ in
+            self?.successCallback?(lock: lock)
         })
     }
     
